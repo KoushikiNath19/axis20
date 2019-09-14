329d728b21a25f2e713941266ba0832abddd9a0b
Hardik gandhi <hg31099@gmail.com>
2019-09-14 08:52:14 +0530

    edit-3(made responsive button)

--- js/main.js
+++ js/main.js
@@ -84,5 +84,26 @@
 			offset: '95%'
 		});
 	});
+	var active1 = false;
+  var active2 = false;
+  var active3 = false;
+  var active4 = false;
+
+    $('.contact-social').on('mousedown touchstart', function() {
+    
+    if (!active1) $(this).find('.t1').css({'transform': 'translate(0px,-125px)'});
+    else $(this).find('.t1').css({'transform': 'none'}); 
+     if (!active2) $(this).find('.t2').css({'transform': 'translate(-60px,-105px)'});
+    else $(this).find('.t2').css({'transform': 'none'});
+      if (!active3) $(this).find('.t3').css({'transform': 'translate(-105px,-60px)'});
+    else $(this).find('.t3').css({'transform': 'none'});
+      if (!active4) $(this).find('.t4').css({'transform': 'translate(-125px,0px)'});
+    else $(this).find('.t4').css({'transform': 'none'});
+    active1 = !active1;
+    active2 = !active2;
+    active3 = !active3;
+    active4 = !active4;
+      
+    });
 
 })(jQuery);
